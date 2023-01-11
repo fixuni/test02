fcf782e2f20d7cd9401f2fec46ccc8f8aec62586
yalariyala <fixuni@users.noreply.github.com>
2023-01-11 15:42:04 +0900

    memo

--- Ex_0102/src/ex1_first/Ex1_print.java
+++ Ex_0102/src/ex1_first/Ex1_print.java
@@ -6,7 +6,7 @@ public class Ex1_print {
 		// 주석 : 컴파일 시 JVM이 인지하지 못하는 메모 용도의 텍스트 영역
 		// main 메서드 : 컴파일 시 가장 먼저 호출되는 영역
 		System.out.println(100); //-< 한줄이 끝나면 반드시 ;으로 마무리
-		System.out.println(200);
+		System.out.println(200);    
 		
 		// 문장을 입력하고자 할때는 ""안에 출력하고자 하는 데이터를 넣어줘야 한다.
 		System.out.println("안녕하세요");
